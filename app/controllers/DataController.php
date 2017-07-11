06ab52090f49aacfa3b3fb3238136b9ddfec50c3
Andrew <awtprod@gmail.com>
2017-07-10 17:42:39 -0700

    yes

--- app/controllers/DataController.php
+++ app/controllers/DataController.php
@@ -281,7 +281,7 @@ class DataController extends \BaseController {
 				$query->where('day_of_week','=',$day);
 			}
 		})->get();
-dd($data);
+
 		$max = 0;
 		foreach ($locations as $location) {
 			$avg_data = array();
